3f4d077048d2535c75a9592e513d67527ae301c9
Rizky <co.terbang@gmail.com>
2022-04-30 02:31:18 +0700

    test pipeline

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
-output "jenkins_terraform" {
-  value = "running Terraform from Jenkins"
+provider "google" {
+  project     = "kapal-private"
+  region      = "asia-southeast2"
+  zone        = "asia-southeast-2b"
+}
+
+resource "google_cloudbuild_trigger" "manual-trigger" {
+  name        = "manual-build"
+
+  source_to_build {
+    uri       = "https://hashicorp/terraform-provider-google-beta"
+    ref       = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+
+  git_file_source {
+    path      = "cloudbuild.yaml"
+    uri       = "https://hashicorp/terraform-provider-google-beta"
+    revision  = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+  approval_config {
+     approval_required = true 
+  }
+
 }
